499b9d7a1d8182ae7d5128a69ea4c66d8b473a54
Robert de Bock <robert@meinit.nl>
2015-10-25 22:04:07 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,4 +3,8 @@
 yum -y localinstall /data/rpmbuild/RPMS/x86_64/*.rpm
 yum -y localinstall /data/rpmbuild/RPMS/noarch/*.rpm
 
+ls -l /etc/init.d/
+echo "==="
+ls -l /etc/rc.d/init.d/
+
 /etc/init.d/apache-tomcat start
