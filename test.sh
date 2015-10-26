41d366f1351d8c02109f506416292cf2834f2a6d
Robert de Bock <robert@meinit.nl>
2015-10-26 13:26:03 +0100

    Update test.sh
    Installing wget, not using lockfiles and being more verbose.

--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@
 
 yum -y localinstall /data/rpmbuild/RPMS/x86_64/apache-tomcat-7.0.65-1.x86_64.rpm
 
-su -p -s /bin/sh apache-tomcat -c "/opt/apache-tomcat/bin/catalina.sh start" > /dev/null && touch /var/lock/apache-tomcat
+su -p -s /bin/sh apache-tomcat -c "/opt/apache-tomcat/bin/catalina.sh start"
 
-sleep 60
+yum -y install wget
 
 wget http://localhost:8080/
 
-su -p -s /bin/sh apache-tomcat -c "/opt/apache-tomcat/bin/catalina.sh stop" > /dev/null && rm -f /var/lock/apache-tomcat
+su -p -s /bin/sh apache-tomcat -c "/opt/apache-tomcat/bin/catalina.sh stop"
