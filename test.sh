741300f63c4148971fabe27d1a3bf8a140343e8e
Robert de Bock <robert@meinit.nl>
2015-10-26 16:10:55 +0100

    Update test.sh
    Adding arguments
    [ci skip]

--- test.sh
+++ test.sh
@@ -1,5 +1,61 @@
 #!/bin/sh
 
-yum -y localinstall /data/rpmbuild/RPMS/x86_64/apache-tomcat-7.0.65-1.x86_64.rpm
+usage() {
+  echo "Usage: $0 -d directory -s SPECfile"
+  echo
+  echo "  -d DIRECTORY"
+  echo "    Directory where the SPECfile exists."
+  echo "  -s SPECfile"
+  echo "    The name of the RPM SPEC file."
+  exit 1
+}
 
-su -p -s /bin/sh apache-tomcat -c "/opt/apache-tomcat/bin/catalina.sh start"
+readargs() {
+  while [ "$#" -gt 0 ] ; do
+    case "$1" in
+      -v)
+        if [ "$2" ] ; then
+          version="$2"
+          shift ; shift
+        else
+          echo "Missing a value for $1."
+          echo
+          shift
+          usage
+        fi
+      ;;
+
+      *)
+        echo "Unknown option or argument $1."
+        echo
+        shift
+        usage
+      ;;
+    esac
+  done
+}
+
+checkargs() {
+  if [ ! "${version}" ] ; then
+    echo "Missing version."
+    usage
+}
+
+setargs() {
+  :
+}
+
+checkvalues() {
+ :
+}
+
+main() {
+  yum -y localinstall /data/rpmbuild/RPMS/x86_64/apache-tomcat-${version}-1.x86_64.rpm
+  su -p -s /bin/sh apache-tomcat -c "/opt/apache-tomcat/bin/catalina.sh start"
+}
+
+readargs "$@" 
+checkargs 
+setargs 
+checkvalues 
+main 
