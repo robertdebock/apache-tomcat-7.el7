e41b477a381dc442c4a2db5d8e2d27a640fc2856
Robert de Bock <robert@meinit.nl>
2015-10-26 13:18:42 +0100

    Update test.sh
    Not using systemd for testing; systemd and Docker are not compatible.

--- test.sh
+++ test.sh
@@ -2,4 +2,10 @@
 
 yum -y localinstall /data/rpmbuild/RPMS/x86_64/apache-tomcat-7.0.65-1.x86_64.rpm
 
-systemctl start apache-tomcat.service
+su -p -s /bin/sh apache-tomcat -c "/opt/apache-tomcat/bin/catalina.sh start" > /dev/null && touch /var/lock/apache-tomcat
+
+sleep 60
+
+wget http://localhost:8080/
+
+su -p -s /bin/sh apache-tomcat -c "/opt/apache-tomcat/bin/catalina.sh stop" > /dev/null && rm -f /var/lock/apache-tomcat
