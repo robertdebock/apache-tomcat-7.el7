b396248657a93a443fc10cb7c081579788a73fa6
Robert de Bock <robert@meinit.nl>
2015-10-26 11:41:33 +0100

    Update test.sh
    Moved packages to one line; seems dependencies are not working as expected.

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-yum -y localinstall /data/rpmbuild/RPMS/x86_64/*.rpm
-yum -y localinstall /data/rpmbuild/RPMS/noarch/*.rpm
+yum -y localinstall /data/rpmbuild/RPMS/{x86_64,noarch}/*.rpm
 
 systemctl start apache-tomcat.service
