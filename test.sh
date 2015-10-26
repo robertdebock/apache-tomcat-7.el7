8ee944237c4b18392408552cd1d3a1a5a0b7f071
Robert de Bock <robert@meinit.nl>
2015-10-26 11:46:59 +0100

    Update test.sh
    Chaning to apache-tomcat installation only.

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-yum -y localinstall /data/rpmbuild/RPMS/{x86_64,noarch}/*.rpm
+yum -y localinstall /data/rpmbuild/RPMS/x86_64/apache-tomcat-7.0.65-1.x86_64.rpm
 
 systemctl start apache-tomcat.service
