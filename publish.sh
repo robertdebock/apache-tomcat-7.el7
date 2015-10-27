5d64a839191b768193d578e83cf11902665283d2
Robert de Bock <robert@meinit.nl>
2015-10-27 20:01:07 +0100

    Create publish.sh
    Initial commit of the publish script.
    [ci skip]

---
+++ publish.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+usage() {
+  echo "Usage: $0 -u FURYURL"
+  echo
+  echo "  -u FURYURL" 
+  echo "    The URL to FURY as provided by FURY. (This includes credentials.)" 
+  echo "  -p PACKAGE"
+  echo "    The package (RPM) to upload."
+  exit 1
+}
+
+readargs() {
+  while [ "$#" -gt 0 ] ; do
+    case "$1" in
+      -u)
+        if [ "$2" ] ; then
+          furyurl="$2"
+          shift ; shift
+        else
+          echo "Missing a value for $1."
+          echo
+          shift
+          usage
+        fi
+      ;;
+      -p)
+        if [ "$2" ] ; then
+          package="$2"
+          shift ; shift
+        else
+          echo "Missing a value for $1."
+          echo
+          shift
+          usage
+        fi
+      ;;
+
+      *)
+        echo "Unknown option or argument $1."
+        echo
+        shift
+        usage
+      ;;
+    esac
+  done
+}
+
+checkargs() {
+  if [ ! "${furyurl}" ] ; then
+    echo "Missing furyurl."
+    echo
+    usage
+  fi
+  if [ ! "${package}" ] ; then
+    echo "Missing package."
+    echo
+    usage
+  fi
+}
+
+setargs() {
+  :
+}
+
+checkvalues() {
+  if [ ! -f ${package} ] ; then
+    echo "Package ${package} does not exist."
+    echo
+    usage
+}
+
+main() {
+  curl -F package=@${package} https://zPx1qa2AWzJmQGcH-L94@push.fury.io/meinitconsultancy/
+}
+
+readargs "$@" 
+checkargs 
+setargs 
+checkvalues 
+main
