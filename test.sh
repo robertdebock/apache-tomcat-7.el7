ba20f8f58d16e56d09692997f4651765c2bad7fb
Robert de Bock <robert@meinit.nl>
2015-10-30 08:14:10 +0100

    Update test.sh
    Added a stop test.

--- test.sh
+++ test.sh
@@ -50,6 +50,7 @@ checkvalues() {
 main() {
   yum -y localinstall /data/rpmbuild/RPMS/x86_64/apache-tomcat-${version}-1.el7.x86_64.rpm
   su -p -s /bin/sh apache-tomcat -c "/opt/apache-tomcat/bin/catalina.sh start"
+  sleep 10  su -p -s /bin/sh apache-tomcat -c "/opt/apache-tomcat/bin/catalina.sh stop"
 }
 
 readargs "$@" 
