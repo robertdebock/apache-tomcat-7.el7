ae10e27ed4cc70fee0b658423c0b5cd9be79c62e
Robert de Bock <robert@meinit.nl>
2015-11-04 14:10:05 +0100

    Update test.sh
    Changed the pattern to exclude debuginfo.

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-yum -y localinstall /data/rpmbuild/RPMS/x86_64/apache-tomcat-*.x86_64.rpm
+yum -y localinstall /data/rpmbuild/RPMS/x86_64/apache-tomcat-?.*.x86_64.rpm
 su -p -s /bin/sh apache-tomcat -c "/opt/apache-tomcat/bin/catalina.sh start"
 sleep 10
 su -p -s /bin/sh apache-tomcat -c "/opt/apache-tomcat/bin/catalina.sh stop"
