65945cd29d402f5882bbfc8d0988428a1a6c349c
Robert de Bock <robert@meinit.nl>
2015-11-05 19:23:05 +0100

    Update build.sh
    Fixed a $ in front of package.

--- build.sh
+++ build.sh
@@ -124,7 +124,7 @@ main() {
   chown root:root ${directory}/${specfile}
   yum -y groupinstall "Development Tools"
   yum -y install  ant java-1.6.0-openjdk java-1.6.0-openjdk-devel
-  cp /data/{package}.service /data/rpmbuild/SOURCES/
+  cp /data/${package}.service /data/rpmbuild/SOURCES/
   rpmbuild --define "_topdir /data/rpmbuild" -ba /data/${package}.spec
 }
 
