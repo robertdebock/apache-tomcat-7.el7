daeee447e283bcce1a620ccdbd1377529fcd9731
Robert de Bock <robert@meinit.nl>
2015-11-05 16:49:06 +0100

    Update prepare.sh
    Moved build.sh in here.
    [ci skip]

--- prepare.sh
+++ prepare.sh
@@ -121,6 +121,11 @@ main() {
     echo
     exit 3
   fi
+  chown root:root ${directory}/${specfile}
+  yum -y groupinstall "Development Tools"
+  yum -y install  ant java-1.6.0-openjdk java-1.6.0-openjdk-devel
+  cp /data/{package}.service /data/rpmbuild/SOURCES/
+  rpmbuild --define "_topdir /data/rpmbuild" -ba /data/${package}.spec
 }
 
 readargs "$@"
