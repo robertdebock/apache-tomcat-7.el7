9b219d022aed97f3445a601b052287aa2fd3d21a
Robert de Bock <robert@meinit.nl>
2015-11-09 15:50:27 +0100

    Update publish.sh
    Including s3 as a destination.

--- publish.sh
+++ publish.sh
@@ -77,12 +77,34 @@ checkvalues() {
   fi
 }
 
-main() {
+gemfury() {
   curl -F package=@${package} ${url}
 }
 
-readargs "$@" 
+s3() {
+  yum -y install openssl
+  file=/data/rpmbuild/RPMS/x86_64/apache-tomcat-7.0.65-1.el7.x86_64.rpm 
+  bucket=apache-tomcat7.el7 
+  region=eu-west-1
+  resource="/${bucket}/${file}"
+  contentType="application/x-compressed-tar"
+  dateValue=`date -R`
+  stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
+  # These variables are stored in Travis.
+  s3Key=${s3key}
+  s3Secret=${secret}
+  signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
+  curl -X PUT -T "${file}" \
+    -H "Host: ${bucket}.s3.amazonaws.com" \
+    -H "Date: ${dateValue}" \
+    -H "Content-Type: ${contentType}" \
+    -H "Authorization: AWS ${s3Key}:${signature}" \
+    https://${bucket}.s3-${region}.amazonaws.com/${file}
+}
+
+readargs "$@"
 checkargs 
 setargs 
 checkvalues 
-main
+gemfury
+s3
