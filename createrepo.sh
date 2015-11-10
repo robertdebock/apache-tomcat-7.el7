e830a99e6d5904b99d81d49f363616f42f96d168
Robert de Bock <robert@meinit.nl>
2015-11-10 13:52:51 +0100

    Update createrepo.sh
    debug.

--- createrepo.sh
+++ createrepo.sh
@@ -6,4 +6,7 @@ mkdir /data/repository/
 cp /data/rpmbuild/RPMS/x86_64/* /data/repository/
 cp /data/rpmbuild/RPMS/noarch/* /data/repository/
 cd /data/repository/
+ls -la
 createrepo .
+ls -la
+ls -la repodata/
