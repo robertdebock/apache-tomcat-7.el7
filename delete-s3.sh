6029aa0bdda412689aff07556ee2fa83aaedcddc
Robert de Bock <robert@meinit.nl>
2015-11-11 11:01:31 +0100

    Update delete-s3.sh
    Removing bucket from URI as described here:
    http://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAPI.html

--- delete-s3.sh
+++ delete-s3.sh
@@ -105,7 +105,7 @@ publish() {
     -H "Host: s3.amazonaws.com" \
     -H "Date: ${dateValue}" \
     -H "Authorization: AWS ${s3Key}:${signature}" \
-    https://${bucket}.s3-${region}.amazonaws.com/${directory}/${file}
+    https://s3-${region}.amazonaws.com/${directory}/${file}
   echo "Done."
 }
 
