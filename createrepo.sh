f26de026ffb767e6ea4f4f59bd6ccbeca5635b2f
Robert de Bock <robert@meinit.nl>
2015-11-12 20:05:27 +0100

    Update createrepo.sh
    Removed debug stuff.

--- createrepo.sh
+++ createrepo.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-
 yum -y install createrepo
 mkdir /data/repository/
 cp /data/rpmbuild/RPMS/x86_64/* /data/repository/
 cp /data/rpmbuild/RPMS/noarch/* /data/repository/
 cd /data/repository/
-ls -la
 createrepo .
-ls -la
-ls -la repodata/
