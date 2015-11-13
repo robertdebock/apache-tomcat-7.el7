9ac31d9b56f267578cd85a419486786f4e902ae0
Robert de Bock <robert@meinit.nl>
2015-11-13 14:49:48 +0100

    Update publish-s3.sh
    Added a delete, because sync does not remove files.

--- publish-s3.sh
+++ publish-s3.sh
@@ -116,6 +116,7 @@ delete() {
   yum -y install epel-release
   yum -y install python-pip
   pip install awscli
+  aws s3 rm se://${bucket}/repodata/ --recursive
   aws s3 sync ${directory} s3://${bucket}
 }
 
