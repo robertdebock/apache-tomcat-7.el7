ec574a5362c4a459b0e71c97d954361cda61806b
Robert de Bock <robert@meinit.nl>
2015-11-13 09:26:10 +0100

    Update createrepo.sh
    Trying to add an index.

--- createrepo.sh
+++ createrepo.sh
@@ -6,3 +6,18 @@ cp /data/rpmbuild/RPMS/x86_64/* /data/repository/
 cp /data/rpmbuild/RPMS/noarch/* /data/repository/
 cd /data/repository/
 createrepo .
+echo << EOF > index.html
+<!DOCTYPE html>
+<html>
+<title>RPM index</title>
+<body>
+EOF
+ 
+ls *.rpm | while read rpm ; do
+ echo "<a href=/${rpm}>${rpm}</a>"
+done
+
+cat << EOF >> index.html
+ </body>
+</html> 
+EOF
