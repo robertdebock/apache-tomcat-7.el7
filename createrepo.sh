4c44b1f58e9c63e221a523b929143d0cdaccaef1
Robert de Bock <robert@meinit.nl>
2015-11-13 12:44:14 +0100

    Update createrepo.sh
    Forgot a redirect.

--- createrepo.sh
+++ createrepo.sh
@@ -6,7 +6,7 @@ cp /data/rpmbuild/RPMS/x86_64/* /data/repository/
 cp /data/rpmbuild/RPMS/noarch/* /data/repository/
 cd /data/repository/
 createrepo .
-echo << EOF > index.html
+echo << EOF >> index.html
 <!DOCTYPE html>
 <html>
 <title>RPM index</title>
@@ -14,7 +14,7 @@ echo << EOF > index.html
 EOF
  
 ls *.rpm | while read rpm ; do
- echo "<a href=/${rpm}>${rpm}</a>"
+ echo "<a href=/${rpm}>${rpm}</a>" >> index.html
 done
 
 cat << EOF >> index.html
